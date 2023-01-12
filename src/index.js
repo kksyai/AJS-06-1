4548103db1e3306a53d0ada672ef1328c8d3aebd
YanaRu <kksyai@gmail.com>
2023-01-12 19:21:25 +0300

    создала массив из объектов

--- src/index.js
+++ src/index.js
@@ -6,4 +6,31 @@ const obj = {
   defence: 40,
 };
 
-console.log(Object.keys(obj));
+const obj = {
+  name: 'мечник',
+  health: 10,
+  level: 2,
+  attack: 80,
+  defence: 40,
+};
+
+function createObj(key, value){
+    this.key = key;
+    this.value = value;
+  }
+const massiv = [];
+
+for (const item in obj) {
+  const newObj = new createObj(item, obj[item]);
+  massiv.push(newObj)
+}
+console.log(massiv)
+
+// на выходе получаю массив из объектов 
+// [
+//  createObj { key: 'name', value: 'мечник' },
+//  createObj { key: 'health', value: 10 },
+//  createObj { key: 'level', value: 2 },
+//  createObj { key: 'attack', value: 80 },
+//  createObj { key: 'defence', value: 40 }
+// ] 
