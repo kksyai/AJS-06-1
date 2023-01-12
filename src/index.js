8b6514a0a99e8ea996f0fc1b9ce9bdbbcce313be
YanaRu <kksyai@gmail.com>
2023-01-12 19:23:03 +0300

    update

--- src/index.js
+++ src/index.js
@@ -6,27 +6,19 @@ const obj = {
   defence: 40,
 };
 
-const obj = {
-  name: 'мечник',
-  health: 10,
-  level: 2,
-  attack: 80,
-  defence: 40,
-};
-
-function createObj(key, value){
-    this.key = key;
-    this.value = value;
-  }
+function createObj(key, value) {
+  this.key = key;
+  this.value = value;
+}
 const massiv = [];
 
 for (const item in obj) {
   const newObj = new createObj(item, obj[item]);
   massiv.push(newObj)
 }
-console.log(massiv)
+console.log(massiv);
 
-// на выходе получаю массив из объектов 
+// на выходе получаю массив из объектов
 // [
 //  createObj { key: 'name', value: 'мечник' },
 //  createObj { key: 'health', value: 10 },
